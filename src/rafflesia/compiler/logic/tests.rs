98a945708ac358bdae03ab469b94b6f67ca3dba5
Iyxan23 <nurihsanalghifari@gmail.com>
2022-06-21 11:43:22 +0700

    test(compiler/logic): add tests for simple statements

--- src/rafflesia/compiler/logic/tests.rs
+++ src/rafflesia/compiler/logic/tests.rs
@@ -64,3 +64,69 @@ button1.onClick {
 
     println!("{:?}", ast);
 }
+
+#[test]
+fn parse_if() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    let code = r#"
+onCreate {
+    idk()
+    if a == b && c.is_cool() {
+        do_something()
+    } else {
+        toast("yeah no")
+    }
+    stuff()
+}
+"#.trim();
+
+    let ast = parse_logic(code)
+        .expect("failed to parse code");
+
+    println!("{:?}", ast);
+}
+
+#[test]
+fn parse_repeat() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    let code = r#"
+onCreate {
+    toast("kinda sus")
+    repeat 10 * 5 + b {
+        toast("rekt")
+    }
+    toast("r")
+}
+"#.trim();
+
+    let ast = parse_logic(code)
+        .expect("failed to parse code");
+
+    println!("{:?}", ast);
+}
+
+#[test]
+fn parse_forever() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    let code = r#"
+boolean a
+
+onCreate {
+    a = true
+
+    forever {
+        break
+        continue
+    }
+    stuff()
+}
+"#.trim();
+
+    let ast = parse_logic(code)
+        .expect("failed to parse code");
+
+    println!("{:?}", ast);
+}
