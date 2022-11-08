f9c9e47a02609943a6f52381e4dd2f098341da26
Iyxan23 <nurihsanalghifari@gmail.com>
2022-11-08 14:01:28 +0700

    feat: initialize the web module

---
+++ rafflesia-web/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+const dist = path.resolve(__dirname, "dist");
+
+module.exports = {
+  experiments: {
+    asyncWebAssembly: true
+  },
+  mode: "production",
+  entry: {
+    index: "./js/index.js"
+  },
+  output: {
+    path: dist,
+    filename: "[name].js",
+  },
+  devServer: {
+    static: dist,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [path.resolve(__dirname, "static")]
+    }),
+
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+    }),
+  ]
+};
