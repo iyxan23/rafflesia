550c1f962e4a1c6c15e75bdd0c4e441bde1112f6
Iyxan23 <nurihsanalghifari@gmail.com>
2023-04-05 11:24:36 +0700

    feat(web): integrate virtualfs to UI (notworking) [noci]

--- rafflesia-web/src/app.rs
+++ rafflesia-web/src/app.rs
@@ -1,30 +1,77 @@
+use std::{cell::RefCell, sync::Arc};
+
 use yew::prelude::*;
+use web_sys::{console, window};
 
-use crate::tree::{Node, Tree};
+use crate::{tree::Tree, template::{virtfs_as_node, self}};
 
-#[function_component(App)]
+// Yew.rs feels like I can just throw any terribly unoptimized code at it
+// and make it seem like nothing. I don't know if doing this is a bad thing
+// there doesn't seem to be any other way of doing this.
+
 pub fn app() -> Html {
-    let root_node = use_state(|| Node {
-        id: "root".into(),
-        name: "rafflesia-project".into(),
-        children: vec![]
+    let fs = use_state(||
+        RefCell::new(template::default())
+    );
+
+    // how to make `root_node` update as `fs` gets updated as well?
+    // does it already do that automatically?
+    let root_node_fs = fs.clone();
+    let root_node =
+        use_memo(|fs|
+            virtfs_as_node("rafflesia-project", root_node_fs),
+            fs.clone()
+        );
+
+    let selected_file = use_state(|| None::<AttrValue>);
+
+    let on_node_click = Callback::from(move |id: AttrValue| {
+        console::log_1(&format!("id: {}", id.as_str()).into());
     });
 
-    let on_node_click = Callback::from(move |id| {
-        println!("pressed: {}", id);
+    let new_file_click_fs = fs.clone();
+    let new_file_click = Callback::from(move |_| {
+        let window = window().unwrap();
+        let Some(name) = window
+            .prompt_with_message("Specify a filename:").unwrap() else {
+                window.alert_with_message("You must specify a filename!").unwrap();
+                return;
+            };
+
+        let Some(path) = window
+            .prompt_with_message("Specify a path:").unwrap() else {
+                window.alert_with_message("You must specify a path!").unwrap();
+                return;
+            };
+        
+        match new_file_click_fs
+            .borrow_mut()
+            .new_file(&path.split("/")
+                .map(ToString::to_string)
+                .collect::<Vec<_>>().as_slice(), path, name, vec![]
+            ) {
+            Ok(_) => window.alert_with_message("Success").unwrap(),
+            Err(err) => window.alert_with_message(&*format!("Error: {:?}", err)).unwrap(),
+        }
     });
 
     html! {
         <div class={classes!("parent")}>
-            <div class={classes!("left-panel")}>
-                <div class={classes!("header")}>
-                    <button>{"New file"}</button>
+            <div class={classes!("top-part")}>
+                <div class={classes!("left-panel")}>
+                    <div class={classes!("header")}>
+                        <button onclick={new_file_click}>{"New file"}</button>
+                    </div>
+                    <Tree click={on_node_click} root_node={root_node.clone()} />
                 </div>
-                <Tree click={on_node_click} root_node={(*root_node).clone()} />
-            </div>
-            <div class={classes!("code")}>
-                <div class={classes!("filename")}>{"main.logic"}</div>
-                <textarea></textarea>
+                <div class={classes!("code")}>
+                    <div class={classes!("filename")}>
+                        if let Some(selected_file) = &*selected_file.clone() {
+                            {selected_file}
+                        } else { {"No file selected"} }
+                    </div>
+                    <textarea></textarea>
+                </div>
             </div>
             <div class={classes!("under")}>
                 {"Hello under"}
--- rafflesia-web/src/tree.rs
+++ rafflesia-web/src/tree.rs
@@ -1,40 +1,93 @@
-use std::ops::Deref;
+use std::sync::Arc;
 
-use yew::{Properties, function_component, html, Html, classes, AttrValue, Callback};
+use yew::{Properties, function_component, html, Html, classes, AttrValue, Callback, use_state};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum NodeKind {
+    Folder {
+        children: Vec<Arc<Node>>
+    },
+    File {
+        selected: bool
+    }
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Node {
     pub id: AttrValue,
     pub name: AttrValue,
-    pub children: Vec<Box<Self>>
+    pub kind: NodeKind
+}
+
+impl Node {
+    pub fn new_file(id: &str, name: &str, selected: bool) -> Self {
+        Node {
+            id: id.to_string().into(),
+            name: name.to_string().into(),
+            kind: NodeKind::File { selected },
+        }
+    }
+
+    pub fn new_folder(id: &str, name: &str, children: Vec<Arc<Self>>) -> Self {
+        Node {
+            id: id.to_string().into(),
+            name: name.to_string().into(),
+            kind: NodeKind::Folder { children }
+        }
+    }
 }
 
 #[derive(Properties, PartialEq)]
 pub struct TreeProps {
-    pub root_node: Node,
+    pub root_node: Arc<Node>,
     pub click: Callback<AttrValue, ()>
 }
 
 #[function_component]
 pub fn Tree(props: &TreeProps) -> Html {
+    let collapse = use_state(|| false);
     let node = &props.root_node;
-    let children = node.children.iter()
-        .map(|child| html! {
-            <Tree click={props.click.clone()} root_node={child.deref().clone()} />
-        }).collect::<Html>();
 
-    let onclick = Callback::from(
-        move |_| todo!() /* props.click.clone().emit(node.id.clone()) */
-    );
+    // at this point i'm just bruteforcing how to solve this issue
+    let click = Arc::new(props.click.clone());
+    let id = node.id.clone();
+
+    let onclick = if let NodeKind::File { .. } = &node.kind {
+        // file
+        Callback::from(move |_| {
+            let id = id.clone();
+            click.emit(id);
+        })
+    } else {
+        let collapse = collapse.clone();
+        // folder, open or close
+        Callback::from(move |_| collapse.set(!*collapse))
+    };
+
+    let contents = if let NodeKind::Folder { children } = &node.kind {
+        html! {
+            <div class={classes!("children")}>
+                {children
+                    .iter()
+                    .map(|child| html! {
+                        <Tree click={props.click.clone()} root_node={child.clone()} />
+                    }).collect::<Html>()}
+            </div>
+        }
+    } else { html! {} };
 
     html! {
         <div class={classes!("node")}>
-            <div {onclick} class={classes!("title")}>
-                {node.name.clone()}
+            <div onclick={onclick} class={classes!("title", if let NodeKind::File { selected } = &node.kind {
+                if *selected { "selected" }
+                else { "" }
+            } else { ""})}>
+                {node.name.clone()} if let NodeKind::Folder { .. } = &node.kind { {"/"} }
             </div>
-            <div class={classes!("children")}>
-                {children}
-            </div>
+
+            if !*collapse {
+                {contents}
+            }
         </div>
     }
 }
